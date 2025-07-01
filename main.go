01128bbfa57cc059eaef69f8b0eb5b34c44a703b
JordanAryaLeksana <jordanaryaleksana41@gmail.com>
2025-07-02 01:04:05 +0700

    initialize golang

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"os"
-	"github.com/gin-gonic/gin"
+    "os"
+    "github.com/gin-gonic/gin"
+    // "github.com/JordanAryaLeksana/station-parfume/config"
+    // "github.com/JordanAryaLeksana/station-parfume/routes"
 )
-func main(){
-	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+
+func main() {
+    // config.ConnectDatabase()
+
+    router := gin.Default()
+
+    api := router.Group("/api/v1")
+
+	api.GET("/", func(c* gin.Context){
+		c.JSON(200, gin.H{"message": "Welcome to Station Parfume API!"})
 	})
-	
-	api := router.Group("/api/v1" )
+    // routes.RegisterRoutes(api) 
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080" 
-	}
-	router.Run(":" + port) 
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
 
+    router.Run(":" + port)
 }
